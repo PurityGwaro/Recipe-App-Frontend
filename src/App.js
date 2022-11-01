2a17ee70f4ea517e49ab058bffa998072609daac
PurityGwaro <puritygwaro@gmail.com>
2022-11-01 22:35:06 +0300

    routing- react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import {Suspense,lazy} from "react"
+import {React, Suspense,lazy} from "react"
 import './App.css';
 import Loader from './components/Loader';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from "react-router-dom";
+
 const Header = lazy(()=> import('./components/Header'))
+const Login = lazy(()=> import('./components/Login'))
 
 function App() {
   return (
+    <>
     <Suspense fallback={<Loader/>}>
       <Header/>
+        <Routes>
+          <Route path="/login" element={<Login/>} />
+        </Routes>
     </Suspense>
+    </>
+    
   );
 }
 
