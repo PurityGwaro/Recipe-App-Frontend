f85e40446a56e8f7604984d93574b4d982045543
PurityGwaro <puritygwaro@gmail.com>
2022-11-11 09:09:48 +0300

    add formik to the login and signup forms

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,45 +1,62 @@
 import React, { useState } from "react";
 import { Box, Button, Typography, TextField } from "@mui/material";
 import { useFormik } from "formik";
-import * as yup from "yup"
-
+import * as yup from "yup";
 
 const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
 // min 5 chars, 1 uppercase, 1 lowercase, 1 numeric
 
 const basicSchema = yup.object().shape({
-  email:yup.string()
+  email: yup
+    .string()
     .email("Please enter a valid email i.e example@gmail.com")
     .required("Required"),
-  username:yup.string()
-    .min(3,"Must be a minimum of 3 characters")
+  username: yup
+    .string()
+    .min(3, "Must be a minimum of 3 characters")
     .required("Required"),
-  password:yup
+  password: yup
     .string()
     .min(5)
-    .matches(passwordRules, {message:"Please create a stronger password. Minimum of 5 characters, must have 1 uppercase letter, 1 lowercase letter and 1 number!"})
+    .matches(passwordRules, {
+      message:
+        "Please create a stronger password. Minimum of 5 characters, must have 1 uppercase letter, 1 lowercase letter and 1 number!",
+    })
     .required("Required"),
-  confirmPassword:yup
+  confirmPassword: yup
     .string()
     .oneOf([yup.ref("password"), null], "Passwords must match")
-    // it's null when it doesn't find it 
-    .required("Required")
-})
+    // it's null when it doesn't find it
+    .required("Required"),
+});
 
 const Auth = () => {
   const [isSignup, setIssignup] = useState(false);
-  const formik = useFormik({
-    initialValues:{
-      email:"",
-      username:"",
-      password:"",
-      confirmPassword:""
+  const onSubmit = async (values, actions) => {
+    console.log("submitting form");
+  };
+  const {
+    values,
+    errors,
+    touched,
+    isSubmitting,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+  } = useFormik({
+    initialValues: {
+      email: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
     },
-  })
+    validationSchema: basicSchema,
+    onSubmit,
+  });
   return (
-    <form>
+    <form onSubmit={handleSubmit} autoComplete="off">
       <Box
-      className="contain-auth"
+        className="contain-auth"
         display="flex"
         flexDirection={"column"}
         alignItems={"center"}
@@ -57,34 +74,84 @@ const Auth = () => {
           padding={3}
           textAlign="center"
         >
-          { isSignup ? "SignUp" : "LogIn" }
+          {isSignup ? "SignUp" : "LogIn"}
         </Typography>
         {isSignup && (
           <>
-            <TextField 
-            id="username"
-            name="username"
-            label="Username" placeholder="UserName" margin="normal" />
+            <TextField
+              id="username"
+              name="username"
+              label="Username"
+              placeholder="UserName"
+              margin="normal"
+              value={values.username}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={
+                errors.username && touched.username ? "input-error" : ""
+              }
+            />
+            {errors.username && touched.username && (
+              <p className="error">{errors.username}</p>
+            )}
           </>
         )}
-        <TextField 
-        id="email"
-        name="email"
-        label="Email" type={"email"} placeholder="Email" margin="normal" />
-        <TextField 
-        id="password1"
-        name="password1"
-        label="Password" type={"password"} placeholder="Password" margin="normal" />
+        <TextField
+          id="email"
+          name="email"
+          label="Email"
+          type={"email"}
+          placeholder="Email"
+          margin="normal"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className={errors.email && touched.email ? "input-error" : ""}
+        />
+        {errors.email && touched.email && (
+          <p className="error">{errors.email}</p>
+        )}
+        <TextField
+          id="password"
+          name="password"
+          label="Password"
+          type={"password"}
+          placeholder="Password"
+          margin="normal"
+          value={values.password1}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className={errors.password && touched.password ? "input-error" : ""}
+        />
+        {errors.password && touched.password && (
+          <p className="error">{errors.password}</p>
+        )}
         {isSignup && (
           <>
-            <TextField 
-            id="confirmPassword"
-            name="confirmPassword"
-            label="Confirm Password" placeholder="Confirm Password" margin="normal" />
+            <TextField
+            type={"password"}
+              id="confirmPassword"
+              name="confirmPassword"
+              label="Confirm Password"
+              placeholder="Confirm Password"
+              margin="normal"
+              value={values.confirmPassword}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={
+                errors.confirmPassword && touched.confirmPassword
+                  ? "input-error"
+                  : ""
+              }
+            />
+            {errors.confirmPassword && touched.confirmPassword && (
+              <p className="error">{errors.confirmPassword}</p>
+            )}
           </>
         )}
         <Button
-        className="btn4"
+          disabled={isSubmitting}
+          className="btn4"
           color="warning"
           variant="contained"
           sx={{
@@ -95,25 +162,23 @@ const Auth = () => {
           Submit
         </Button>
 
-       
-          <Button
+        <Button
           className="btn5"
-            onClick={() => {
-              setIssignup(!isSignup);
-            }}
-            sx={{
-              marginTop: 2,
-              marginBottom: 2,
-              borderRadius: "8px",
-              textDecoration: "underline",
-              fontSize: "13px",
-              fontWeight: "600",
-              textTransform: "capitalize",
-            }}
-          >
-            Change To {isSignup ? "Log In" : "Sign Up"}
-          </Button>
-      
+          onClick={() => {
+            setIssignup(!isSignup);
+          }}
+          sx={{
+            marginTop: 2,
+            marginBottom: 2,
+            borderRadius: "8px",
+            textDecoration: "underline",
+            fontSize: "13px",
+            fontWeight: "600",
+            textTransform: "capitalize",
+          }}
+        >
+          Change To {isSignup ? "Log In" : "Sign Up"}
+        </Button>
       </Box>
     </form>
   );
