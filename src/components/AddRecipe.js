0a98429729bb969b54f895dad1cd723fa4212237
PurityGwaro <puritygwaro@gmail.com>
2022-11-16 17:18:02 +0300

    formim errors for the title, resized the input for steps

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -35,7 +35,11 @@ const alpha_num_limited = {
 
 // --- Form Schema Validation Definition -- //
 const FormValidationSchema = yup.object().shape({
-  item_name: yup
+  title: yup
+    .string()
+    .min(1)
+    .required("You Must Provide A Title For Your Recipe!"),
+  step_text: yup
     .string()
     .matches(alpha_num_limited.regex, alpha_num_limited.message),
   // quantity: yup.number().typeError("Please enter a numebr"),
@@ -43,7 +47,8 @@ const FormValidationSchema = yup.object().shape({
 
 // --- Form Schema  Definition -- //
 const FormSchema = {
-  item_name: "",
+  title: "",
+  step_text: "",
   items: [],
 };
 
@@ -66,268 +71,202 @@ const AddRecipe = () => {
     onSubmit: async (values, actions) => {},
   });
   return (
-    
-        <Container
-        
-        style={{
-          margin:"auto",
-        marginTop:"5%",        
-        padding:"2%",
-        paddingRight:"4%",
-        paddingLeft:"4%",
-        paddingBottom:"4%",
-        width:"50%",
-        display:"flex",
-        flexDirection:"column",
+    <Container
+      style={{
+        margin: "auto",
+        marginTop: "5%",
+        padding: "2%",
+        paddingRight: "4%",
+        paddingLeft: "4%",
+        paddingBottom: "4%",
+        width: "50%",
+        display: "flex",
+        flexDirection: "column",
         // border={3}
         // borderColor="#ccc"
-        borderRadius:"4%",
-        boxShadow:"10px 10px 20px #ccc",
-        borderTop:"2px solid #ed6c02",
-        }}
-        >
-          <Formik
-            initialValues={FormSchema}
-            validationSchema={FormValidationSchema}
-            render={(props,arrayHelpers) => (
-              <Form>
-                <Typography
-          fontWeight={"bolder"}
-          // fontSize={30}
-          variant="h4"
-          padding={2}
-          textAlign={"center"}
-        >
-          Post Your Recipe
-        </Typography>
-                {/* {console.log(props)} */}
-                <InputLabel className="label">Title</InputLabel>
-                <TextField
-                  margin="auto"
-                  variant="outlined"
-                  size="small"
-                  sx={{ width: "90%" }}
-                />
-                <Button
-                    variant="contained"
-                    component="label"
-                    color="warning"
-                    sx={{
-                      borderRadius: "8px",
-                      marginTop: 3,
-                      width: "20%",
-                    }}
-                  >
-                    Upload Image
-                    <input hidden accept="image/*" multiple type="file" />
-                  </Button>
-                <InputLabel className="label">Procedure</InputLabel>
+        borderRadius: "4%",
+        boxShadow: "10px 10px 20px #ccc",
+        borderTop: "2px solid #ed6c02",
+      }}
+    >
+      <Formik
+        initialValues={FormSchema}
+        validationSchema={FormValidationSchema}
+        // render=
+      >
+        {(props, arrayHelpers) => (
+          <Form>
+            <Typography
+              fontWeight={"bolder"}
+              // fontSize={30}
+              variant="h4"
+              padding={2}
+              textAlign={"center"}
+            >
+              Post Your Recipe
+            </Typography>
+            {/* {console.log(props)} */}
+            <InputLabel className="label">Title</InputLabel>
 
-                <FieldArray name="items"
-                render={(arrayHelpers) => (
-                  <List>
-                  { props.values.items.map((item,index) => {
+            <TextField
+              id="title"
+              name="title"
+              // label="title"
+              fullWidth
+              size="small"
+              variant="outlined"
+              value={props.values.title}
+              helperText={
+                props.touched.title && props.errors.title
+                  ? props.errors.title
+                  : " "
+              }
+              error={props.touched.title && Boolean(props.errors.title)}
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              InputLabelProps={{ shrink: true }}
+            />
+            <Button
+              variant="contained"
+              component="label"
+              color="warning"
+              sx={{
+                borderRadius: "8px",
+                marginTop: 3,
+                width: "20%",
+              }}
+            >
+              Upload Image
+              <input hidden accept="image/*" multiple type="file" />
+            </Button>
+            <InputLabel className="label">Procedure</InputLabel>
+
+            <FieldArray name="items">
+              {(arrayHelpers) => (
+                <List>
+                  {props.values.items.map((item, index) => {
                     return (
-                      <>
-                      <ListItem  key={index}>
-                        
+                      <ListItem key={index}>
                         <TextField
-                        value={item.item_name}
-                        placeholder={item.item_name}
-                        onChange={()=>{console.log('editing a step');}}
+                          value={item.step_text}
+                          placeholder={item.step_text}
+                          onChange={() => {
+                            console.log("editing a step");
+                          }}
                         />
                         <Button
-                      type="button"
-                      onClick={() =>{
-                        console.log(`delete button clicked, to remove item of index ${index}`);
-                        // const removed = arrayHelpers.remove(index)
-                        console.log(arrayHelpers);
-                      }}
-                      >Edit</Button>
+                          type="button"
+                          onClick={() => {
+                            console.log(
+                              `delete button clicked, to remove item of index ${index}`
+                            );
+                            console.log(arrayHelpers);
+                          }}
+                        >
+                          Edit
+                        </Button>
                         <Button
-                      type="button"
-                      onClick={() =>{
-                        console.log(`delete button clicked, to remove item of index ${index}`);
-                        const removed = arrayHelpers.remove(index)
-                        console.log(typeof removed);
-                      }}
-                      >Delete</Button>
+                          type="button"
+                          onClick={() => {
+                            console.log(
+                              `delete button clicked, to remove item of index ${index}`
+                            );
+                            arrayHelpers.remove(index);
+                          }}
+                        >
+                          Delete
+                        </Button>
                       </ListItem>
-                      
-                      
-                      </>
-                      
                     );
                   })}
                 </List>
-                )}
-                />
-       
-                {/* <Box 
-                    // minHeight="200px"
-                    // maxHeight="200px"
-                    // overflow="scroll"
-                    >
-                     
-                      <List>
-                        { props.values.items.map((item,index) => {
-                          return (
-                            <>
-                            <ListItem key={index}>
-                              
-                              <ListItemText
-                              name={`item.${index}`}
-                              primary={item.item_name}
-                              />
-                              <Button
-                            type="button"
-                            onClick={() =>{
-                              console.log(`delete button clicked, to remove item of index ${index}`);
-                              console.log(`initial value b4 delete ${item.item_name}`);
-                              // props.setValues({
-                              //   items: values.items.filter((item) => item !== index), // Remove clicked item
-                              // })
-                              
-                              props.validateForm().then((results) => {
-                                if (!("item_name" in results)) {
-                                  // Sets the item field
-                                  props.setValues({
-                                items: values.items.filter((item) => item !== index), // Remove clicked item
-                              })
-      
-                                  // Clear form values
-                                }
-                              });
-                              console.log(`initial value after delete ${item.item_name}`);
-                            }}
-                            >Delete</Button>
-                            </ListItem>
-                            
-                            </>
-                            
-                          );
-                        })}
-                      </List>
-                    </Box> */}
-               
-                
+              )}
+            </FieldArray>
 
-                <Box
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Box item sx={{ width: "90%" }}>
+                <TextField
+                  id="step_text"
+                  name="step_text"
+                  label="Step"
+                  fullWidth
+                  size="small"
+                  variant="outlined"
+                  value={props.values.step_text}
+                  helperText={
+                    props.touched.step_text && props.errors.step_text
+                      ? props.errors.step_text
+                      : " "
+                  }
+                  error={
+                    props.touched.step_text && Boolean(props.errors.step_text)
+                  }
+                  onChange={props.handleChange}
+                  onBlur={props.handleBlur}
+                  InputLabelProps={{ shrink: true }}
+                />
+              </Box>
+              {/* add button start */}
+              <Box item>
+                <IconButton
                   sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
+                    border: "2px solid #ed6c02",
+                    borderRadius: "8px",
+                    color: "#ed6c02",
+                    fontSize: "90px",
+                    background: "#ccc",
+                  }}
+                  type="button"
+                  onClick={() => {
+                    console.log("Add button clicked");
+                    if (props.values.step_text.length > 0) {
+                      props.validateForm().then((results) => {
+                        if (!("step_text" in results)) {
+                          // Sets the item field
+                          props.setFieldValue("items", [
+                            ...props.values.items,
+                            {
+                              step_text: props.values.step_text,
+                            },
+                          ]);
+
+                          // Clear form values
+                          props.setFieldValue("step_text", "");
+                        }
+                      });
+                    }
                   }}
                 >
-                  <Box item sx={{ width: "90%" }}>
-                    <TextField
-                      id="item_name"
-                      name="item_name"
-                      label="Step"
-                      fullWidth
-                      variant="outlined"
-                      value={props.values.item_name}
-                      helperText={
-                        props.touched.item_name && props.errors.item_name
-                          ? props.errors.item_name
-                          : " "
-                      }
-                      error={
-                        props.touched.item_name &&
-                        Boolean(props.errors.item_name)
-                      }
-                      onChange={props.handleChange}
-                      onBlur={props.handleBlur}
-                      InputLabelProps={{ shrink: true }}
-                    />
-                  </Box>
-                  {/* add button start */}
-                  <Box item>
-                    <IconButton
-                      sx={{
-                        border: "2px solid #ed6c02",
-                        borderRadius: "8px",
-                        color: "#ed6c02",
-                        fontSize: "90px",
-                        background: "#ccc",
-                      }}
-                      type="button"
-                      onClick={() => {
-                        console.log("Add button clicked");
-
-                        props.validateForm().then((results) => {
-                          if (!("item_name" in results)) {
-                            // Sets the item field
-                            props.setFieldValue("items", [
-                              ...props.values.items,
-                              {
-                                item_name: props.values.item_name,
-                              },
-                            ]);
-
-                            // Clear form values
-                            props.setFieldValue("item_name", "");
-                          }
-                        });
-                      }}
-                    >
-                      <AddIcon sx={{ fontSize: "30px" }} />
-                    </IconButton>
-                  </Box>
-                  {/* add button end */}
-
-                  
-                </Box>
-               
-                
-              </Form>
-            )}
-          />
-
-            {/* <Box
-          sx={{
-            display: "flex",
-          }}
-        >
-          <TextField
-            margin="auto"
-            variant="outlined"
-            size="small"
-            placeholder="Add Step"
-            sx={{
-              width: "90%",
-            }}
-          />
-          <Button
-            sx={{
-              background: "#ccc",
-              padding: "0",
-              marginLeft: "15px",
-            }}
-          >
-            <AddIcon sx={{ color: "#ed6c02", fontSize: "30px" }} />
-          </Button>
-        </Box> */}
-
-        <Box textAlign="center">
-          <Button
-            type="submit"
-            className="btn4"
-            color="warning"
-            variant="contained"
-            sx={{
-              borderRadius: "8px",
-              marginTop: 5,
-              width: "20%",
-            }}
-          >
-            POST
-          </Button>
-        </Box>
-        </Container>
-
-      
-  
+                  <AddIcon sx={{ fontSize: "30px" }} />
+                </IconButton>
+              </Box>
+              {/* add button end */}
+            </Box>
+            <Box textAlign="center">
+              <Button
+                type="submit"
+                className="btn4"
+                color="warning"
+                variant="contained"
+                sx={{
+                  borderRadius: "8px",
+                  marginTop: 5,
+                  width: "20%",
+                }}
+              >
+                POST
+              </Button>
+            </Box>
+          </Form>
+        )}
+      </Formik>
+    </Container>
   );
 };
 
